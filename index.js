7a1d23d913a30ae0b4a7012ee94cbd40ab4d42d2
yousuf <gourabxz@gmail.com>
2021-08-03 23:36:34 +0600

    test download file

--- index.js
+++ index.js
@@ -1,50 +1,31 @@
 const express = require('express')
-var os = require('os');
-var fs = require('fs');
-
-const path = require('path');
-const { resolve } = require('path');
+const path = require("path");
 const PORT = process.env.PORT || 5000
-
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => res.render('pages/index'))
+app.get('/', (req, res) => res.render('pages/index', {fileSize: "100"}));
 
+app.use("/download-test-file", (req, res, next) => {
+  var filePath = path.join(__dirname, "app-assets", "tiktoksong.mp3");
 
-app.get("/cpu-info", (req, res) => {
-  var cpuInfo = {
-    cpus: os.cpus(),
-    totalmem: os.totalmem(),
-    freemem: os.freemem()
-  };
-  res.json(cpuInfo)
-})
-
-
-app.get("/read-to-home", (req, res) => {
-  var userHome = os.homedir();
-  var fileWriteResp = "";
-
-
-  fs.writeFile(userHome + '/helloworld.txt', 'Hello World!', function (err) {
-    if (err) {
-      fileWriteResp = "file writting failed";
-      res.json({
-        userHome: userHome,
-        fileWriteResp: fileWriteResp
-      });
+  res.sendFile(filePath, (err)=>{
+    if(err){
+      next(err);
+    }else{
+      console.log("mp3 file sent");
+      next();
     }
-    fileWriteResp = "file writting success";
-    res.json({
-      userHome: userHome,
-      fileWriteResp: fileWriteResp
-    });
-  });
+  })
 });
+
+app.get("/download-test-file", (req, res) => {
+  console.log("File sent");
+  res.send();
+})
 
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
